f45d0c8db7e6f8f8ac32abbdd434e87d3943decc
jkdlps <119383896+jkdlps@users.noreply.github.com>
2023-02-28 13:04:06 +0000

    modified:   login.php

--- login.php
+++ login.php
@@ -7,3 +7,19 @@ include "header.php";
         <button type="submit">Back to Homepage</button>
     </form>
 </div>
+
+<div>
+    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+        <label for="email">Email:</label>
+        <input type="email" name="email" id="email">
+        <label for="password">Password:</label>
+        <input type="password" name="password" id="password">
+        <button type="submit">Log In</button>
+    </form>
+</div>
+
+<?php
+
+if($_SERVER['POST']) {
+    echo "test";
+}
