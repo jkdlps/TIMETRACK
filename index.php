19435868ecba4577fcd9c2fabf14c5efa62ccf0d
jkdlps <119383896+jkdlps@users.noreply.github.com>
2023-02-28 11:56:19 +0000

    restart

--- index.php
+++ index.php
@@ -5,13 +5,17 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>TIMETRACK</title>
-
-    <!-- water.css -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
 </head>
 <body>
     <div>
         <h1>TIMETRACK: GPS Timekeeping System</h1>
         <p>Developed for Municipal Office of Barangay Pulong Buhangin, Santa Maria, Bulacan</p>
+        <hr>
     </div>
-    <hr>
+    <div>
+        <form action="login.php" method="get">
+            <button type="submit">Log In</button>
+        </form>
+    </div>
+</body>
+</html>
