626fc36a961383cb943a9381f74c1052acdcad4d
jkdlps <119383896+jkdlps@users.noreply.github.com>
2023-02-28 12:38:22 +0000

    modified:   index.php

--- index.php
+++ index.php
@@ -3,7 +3,23 @@ include "header.php";
 ?>
 
 <div>
-    <hr>
+    <h3>Features:</h3>
+    <ul>
+        <li>Geolocation & Geofencing Attendance</li>
+        <li>Printable Daily Time Record Generation</li>
+        <li>Employee & Employer Panels</li>
+        <li>One Time Passcode Verification through Email</li>
+    </ul>
+</div>
+
+<div>
+    <form action="login.php" method="post">
+        <button type="submit">Log In</button>
+    </form>
+</div>
+
+<div>
+    <h2>Documentation</h2>
     <div>
         <form action="desc_schema.php" method="get">
             <button type="submit">Read System Schema</button>
@@ -14,7 +30,16 @@ include "header.php";
             <button type="submit">Read System Structure</button>
         </form>
     </div>
-    <hr>
+</div>
+
+<div>
+    <h4>Created by BSIT 4-1 Group 6 consisting of:</h4>
+    <ul>
+        <li>Casimiro, Alvin as Data Analyst</li>
+        <li>Del Poso, James Kevin as Programmer</li>
+        <li>Dimanlig, Rafaelle as Researcher</li>
+        <li>Marqueta, Marq Joshua as Project Manager</li>
+    </ul>
 </div>
 
 </body>
