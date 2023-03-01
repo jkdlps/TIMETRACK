fe6c1391a46c43b36671a1d64c10a012abfcfee1
jkdlps <119383896+jkdlps@users.noreply.github.com>
2023-03-01 09:23:45 +0000

    modified:   functions.php 	modified:   login.php

--- functions.php
+++ functions.php
@@ -5,3 +5,10 @@ function sanitize($data) {
     $data = htmlspecialchars($data);
     return $data;
 }
+
+function query ($sql, $data=null) {
+    include "conn.php";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("$bind", null);
+    $stmt->execute($data);
+}
--- login.php
+++ login.php
@@ -1,3 +1,8 @@
+<?php
+include "header.php";
+include "conn.php";
+?>
+
 <form action="login.php" method="post">
   <label for="email">Email:</label>
   <input type="email" id="email" name="email" required>
@@ -10,48 +15,3 @@
 
   <input type="submit" value="Log In">
 </form>
-
-<?php
-// Connect to database
-$db = new PDO('mysql:host=localhost;dbname=mydatabase', 'username', 'password');
-
-// Prepare SQL statement
-$stmt = $db->prepare('SELECT email, password, role FROM users WHERE email = :email');
-$stmt->bindParam(':email', $_POST['email']);
-$stmt->execute();
-
-// Fetch user data
-$user = $stmt->fetch(PDO::FETCH_ASSOC);
-
-// Check password
-if ($user && password_verify($_POST['password'], $user['password'])) {
-  // Password is correct, log user in
-} else {
-  // Password is incorrect, show error message
-}
-
-
-if ($user && password_verify($_POST['password'], $user['password'])) {
-  // Password is correct, log user in
-  $_SESSION['role'] = $user['role'];
-  if ($_SESSION['role'] == 1) {
-    // Employer, redirect to employer dashboard
-    header('Location: employer_dashboard.php');
-    exit();
-  } else {
-    // Employee, redirect to employee dashboard
-    header('Location: employee_dashboard.php');
-    exit();
-  }
-} else {
-  // Password is incorrect, show error message
-  echo 'Invalid email or password.';
-}
-
-if ($_POST['remember']) {
-    // Remember user for 1 week
-    setcookie('email', $_POST['email'], time() + 60*60*24*7);
-    setcookie('password', $_POST['password'], time() + 60*60*24*7);
-  }
-
-  
