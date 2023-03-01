aeadd99d90b6747b131db3dff1b8755276a1d81c
jkdlps <119383896+jkdlps@users.noreply.github.com>
2023-03-01 17:17:21 +0000

    new file:   employee_dashboard.php 	modified:   index.php 	modified:   login.php 	modified:   set_aside/structure.txt

--- login.php
+++ login.php
@@ -10,10 +10,86 @@
       header("Location: employee_dashboard.php");
     }
   }
-
-  if(isset($_POST['submit'])) {
-    echo "doge";
+  
+  // Handle form submission
+  if (isset($_POST['submit'])) {
+      $email = $_POST['email'];
+      $password = $_POST['password'];
+      $remember = isset($_POST['remember']) ? $_POST['remember'] : 0;
+  
+      // Check if user is an employee or employer
+      $query = "SELECT * FROM users WHERE email = ?";
+      $stmt = $conn->prepare($query);
+      $stmt->bind_param("s", $email);
+      $stmt->execute();
+      $result = $stmt->get_result();
+  
+      if ($result->num_rows == 1) {
+          $row = $result->fetch_assoc();
+          if (password_verify($password, $row['password'])) {
+              // Password is correct, check if user has been validated with a one-time passcode
+              if ($row['validated'] == 1) {
+                  // User is validated, set session variables and redirect to dashboard
+                  $_SESSION['user_id'] = $row['id'];
+                  $_SESSION['user_type'] = $row['user_type'];
+                  if ($remember == 1) {
+                      setcookie("user_id", $row['id'], time() + (86400 * 30), "/");
+                      setcookie("user_type", $row['user_type'], time() + (86400 * 30), "/");
+                  }
+                  if($row['role'] == 0) {
+                    header("Location: employee_dashboard.php");
+                    exit();
+                  } elseif($row['role'] == 1) {
+                    header("Location: employer_dashboard.php");
+                    exit();
+                  }
+              } else {
+                  // User needs to be validated with a one-time passcode
+                  $_SESSION['temp_user_id'] = $row['id'];
+                  $_SESSION['temp_user_type'] = $row['user_type'];
+                  header("Location: validate.php");
+                  exit();
+              }
+          }
+      }  
+      // If user is not found or password is incorrect, show error message
+      $error_message = "Invalid email or password.";
   }
+  
+  // Handle forgot password request
+  if (isset($_POST['forgot_password'])) {
+      $email = $_POST['email'];
+  
+      // Check if email exists in database
+      $query = "SELECT * FROM users WHERE email = ?";
+      $stmt = $conn->prepare($query);
+      $stmt->bind_param("s", $email);
+      $stmt->execute();
+      $result = $stmt->get_result();
+  
+      if ($result->num_rows == 1) {
+          // Generate new password and update database
+          $new_password = bin2hex(random_bytes(4));
+          $hashed_password = password_hash($new_password, PASSWORD_DEFAULT);
+          $query = "UPDATE users SET password = ? WHERE email = ?";
+          $stmt = $conn->prepare($query);
+          $stmt->bind_param("ss", $hashed_password, $email);
+          $stmt->execute();
+  
+          // Send email with new password to user
+          $to = $email;
+          $subject = "New password for your account";
+          $message = "Your new password is: " . $new_password;
+          $headers = "From: webmaster@example.com";
+          mail($to, $subject, $message, $headers);
+  
+          $success_message = "A new password has been sent to your email.";
+      } else {
+          $error_message = "Invalid email.";
+      }
+  }
+  
+  
 ?>
 
 <div>
