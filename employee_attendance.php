3d0350124fb81707ea25bf37039685a0ec53508e
jkdlps <119383896+jkdlps@users.noreply.github.com>
2023-03-02 16:12:48 +0000

    modified:   employee_attendance.php

--- employee_attendance.php
+++ employee_attendance.php
@@ -2,60 +2,6 @@
 include "conn.php";
 session_start();
 ?>
-    <!-- <script>
-      function getLocation() {
-        if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(showPosition);
-        } else {
-          alert("Geolocation is not supported by this browser.");
-        }
-      }
-
-      function showPosition(position) {
-        // Get the latitude and longitude
-        var lat = position.coords.latitude;
-        var lon = position.coords.longitude;
-
-        // Check if the location is within the geofence
-        if (lat > 14.795208 && lat < 14.799445 && lon > 121.070042 && lon < 121.073590) {
-          document.getElementById("location").value = lat + "," + lon;
-          document.getElementById("status").innerHTML = "In office";
-          document.getElementById("in_office").value = "1";
-        } else {
-          document.getElementById("location").value = lat + "," + lon;
-          document.getElementById("status").innerHTML = "Work from home";
-          document.getElementById("in_office").value = "0";
-        }
-      }
-    </script>
-    <h2>Employee Attendance</h2>
-    <form method="post" action="submit_attendance.php">
-      <label>Date:</label>
-      <input type="date" name="date" value="<?php echo date('Y-m-d'); ?>" readonly>
-      <br>
-      <label>Time:</label>
-      <input type="time" name="time" value="<?php echo date('H:i'); ?>" readonly>
-      <br>
-      <label>Location:</label>
-      <input type="text" name="location" id="location" readonly>
-      <button type="button" onclick="getLocation()">Get Location</button>
-      <br>
-      <label>Status:</label>
-      <span id="status"></span>
-      <input type="hidden" name="in_office" id="in_office" value="">
-      <br>
-      <input type="submit" value="Submit">
-    </form>
-
-    <div>
-        <form action='employee_dashboard.php' method='post'>
-            <button type='submit'>Back to Dashboard</button>
-        </form>
-    </div>
-
-  </body>
-</html> -->
-
 <script>
 		function getLocation() {
 			if (navigator.geolocation) {
@@ -94,6 +40,7 @@ session_start();
 			alert("Attendance recorded:\nStatus: " + status + "\nDate and Time: " + datetime);
 
 			// TODO: save record to database
+		}
 
 		function getDistance(lat1, lon1, lat2, lon2) {
 			var R = 6371; // Radius of the earth in km
@@ -112,43 +59,4 @@ session_start();
 		function deg2rad(deg) {
 			return deg * (Math.PI/180)
 		}
-	}
 	</script>
-<?php 
-// Get the form data
-$date = $_POST['date'];
-$time = $_POST['time'];
-$location = $_POST['location'];
-$in_office = $_POST['in_office'];
-$user_id = $_SESSION['user_id'];
-
-// Convert the time to Asia/Manila timezone
-$datetime = new DateTime($date . ' ' . $time, new DateTimeZone('UTC'));
-$datetime->setTimezone(new DateTimeZone('Asia/Manila'));
-$timestamp = $datetime->format('Y-m-d H:i:s');
-
-// Check if the user has already timed in
-$sql = "SELECT * FROM attendance WHERE user_id='$user_id' AND time_out IS NULL";
-$result = mysqli_query($conn, $sql);
-
-if (mysqli_num_rows($result) > 0) {
-  // User has already timed in, update the time_out column
-  $row = mysqli_fetch_assoc($result);
-  $id = $row['id'];
-  $sql = "UPDATE attendance SET time_out='$timestamp' WHERE id='$id'";
-  mysqli_query($conn, $sql);
-} else {
-  // User has not timed in yet, insert a new record
-  $sql = "INSERT INTO attendance (user_id, time_in, location, in_office) VALUES ('$user_id', '$timestamp', '$location', '$in_office')";
-  mysqli_query($conn, $sql);
-}
-
-mysqli_close($conn);
-
-// Redirect to the dashboard
-if($_SESSION['role'] == 1) {
-    header('location: employer_dashboard.php');
-} elseif($_SESSION['role'] == 0) {
-    header('location: employee_dashboard.php');
-}
-exit();
