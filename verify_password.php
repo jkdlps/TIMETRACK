de40de75e4ca8d3bfb0bd02e4e7db73f35e73490
jkdlps <119383896+jkdlps@users.noreply.github.com>
2023-03-02 06:22:43 +0000

    modified:   verify_password.php

--- verify_password.php
+++ verify_password.php
@@ -2,10 +2,10 @@
 session_start();
 
 // Check if user is logged in
-// if (!isset($_SESSION['user_id'])) {
-//     header("Location: login.php");
-//     exit();
-// }
+if (!isset($_SESSION['user_id'])) {
+    header("Location: login.php");
+    exit();
+}
 
 // Check if form has been submitted
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
