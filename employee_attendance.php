559e4b38c4d4f273eba5b66b52b393230792891b
jkdlps <119383896+jkdlps@users.noreply.github.com>
2023-03-02 16:11:22 +0000

    modified:   employee_attendance.php

--- employee_attendance.php
+++ employee_attendance.php
@@ -1,5 +1,6 @@
 <?php
 include "conn.php";
+session_start();
 ?>
     <!-- <script>
       function getLocation() {
@@ -113,3 +114,41 @@ include "conn.php";
 		}
 	}
 	</script>
+<?php 
+// Get the form data
+$date = $_POST['date'];
+$time = $_POST['time'];
+$location = $_POST['location'];
+$in_office = $_POST['in_office'];
+$user_id = $_SESSION['user_id'];
+
+// Convert the time to Asia/Manila timezone
+$datetime = new DateTime($date . ' ' . $time, new DateTimeZone('UTC'));
+$datetime->setTimezone(new DateTimeZone('Asia/Manila'));
+$timestamp = $datetime->format('Y-m-d H:i:s');
+
+// Check if the user has already timed in
+$sql = "SELECT * FROM attendance WHERE user_id='$user_id' AND time_out IS NULL";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+  // User has already timed in, update the time_out column
+  $row = mysqli_fetch_assoc($result);
+  $id = $row['id'];
+  $sql = "UPDATE attendance SET time_out='$timestamp' WHERE id='$id'";
+  mysqli_query($conn, $sql);
+} else {
+  // User has not timed in yet, insert a new record
+  $sql = "INSERT INTO attendance (user_id, time_in, location, in_office) VALUES ('$user_id', '$timestamp', '$location', '$in_office')";
+  mysqli_query($conn, $sql);
+}
+
+mysqli_close($conn);
+
+// Redirect to the dashboard
+if($_SESSION['role'] == 1) {
+    header('location: employer_dashboard.php');
+} elseif($_SESSION['role'] == 0) {
+    header('location: employee_dashboard.php');
+}
+exit();
