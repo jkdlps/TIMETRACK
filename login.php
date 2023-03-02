37ab1b00c822b3b929ed24627c6de9fb085baf18
jkdlps <119383896+jkdlps@users.noreply.github.com>
2023-03-02 08:49:27 +0000

    modified:   login.php

--- login.php
+++ login.php
@@ -1,89 +1,95 @@
 <?php
-session_start(); // start session
+session_start();
+require_once 'header.php';
 
-// check if user is already logged in
-if(isset($_SESSION["user_id"])) {
-  // redirect to dashboard based on account type
-  if($_SESSION["role"] == 1) {
-    header("Location: employer_dashboard.php");
-  } else {
-    header("Location: employee_dashboard.php");
-  }
-  exit;
+// Check if user is already logged in
+if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+    if($_SESSION["role"] == 1) {
+        header("location: employer_dashboard.php");
+    } else {
+        header("location: employee_dashboard.php");
+    }
+    exit;
 }
 
-// include database connection
-include_once "header.php";
-
-// define variables and set to empty values
 $email = $password = "";
 $email_err = $password_err = "";
 
-// process form data when form is submitted
-if($_SERVER["REQUEST_METHOD"] == "POST") {
-  
-  // validate email
-  if(empty(sanitize($_POST["email"]))) {
-    $email_err = "Please enter your email.";
-  } else {
-    $email = sanitize($_POST["email"]);
-  }
-  
-  // validate password
-  if(empty(sanitize($_POST["password"]))) {
-    $password_err = "Please enter your password.";
-  } else {
-    $password = sanitize($_POST["password"]);
-  }
-
-  // if no errors, attempt login
-  if(empty($email_err) && empty($password_err)) {
-    $sql = "SELECT id, email, password, role FROM employee WHERE email = ?";
-    
-    if($stmt = $conn->prepare($sql)) {
-      $stmt->bind_param("s", $param_email);
-      $param_email = $email;
-      
-      if($stmt->execute()) {
-        $stmt->store_result();
-        
-        // check if email exists
-        if($stmt->num_rows == 1) {
-          $stmt->bind_result($id, $email, $hashed_password, $role);
-          if($stmt->fetch()) {
-            // verify password
-            // if(password_verify($password, $hashed_password)) {
-              // password is correct, start session
-              session_start();
-              $_SESSION["user_id"] = $id;
-              $_SESSION["email"] = $email;
-              $_SESSION["role"] = $role;
-              
-              // redirect to dashboard based on account type
-              if($role == 1) {
-                header("Location: employer_dashboard.php");
-              } else {
-                header("Location: employee_dashboard.php");
-              }
-            } else {
-              // password is incorrect
-              $password_err = "The password you entered is not valid.";
-            }
-          }
-        } else {
-          // email not found
-          $email_err = "No account found with that email.";
-        }
-      } else {
-        // query failed
-        echo "Oops! Something went wrong. Please try again later.";
-      }
-      
-      $stmt->close();
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+ 
+    // Check if email is empty
+    if(empty(trim($_POST["email"]))){
+        $email_err = "Please enter email.";
+    } else{
+        $email = trim($_POST["email"]);
     }
-  // }
-  
-  $conn->close();
+    
+    // Check if password is empty
+    if(empty(trim($_POST["password"]))){
+        $password_err = "Please enter your password.";
+    } else{
+        $password = trim($_POST["password"]);
+    }
+    
+    // Validate credentials
+    if(empty($email_err) && empty($password_err)){
+        $sql = "SELECT id, employee_id, name, email, password, role FROM users WHERE email = ?";
+        
+        if($stmt = mysqli_prepare($conn, $sql)){
+            mysqli_stmt_bind_param($stmt, "s", $param_email);
+            
+            $param_email = $email;
+            
+            if(mysqli_stmt_execute($stmt)){
+                mysqli_stmt_store_result($stmt);
+                
+                // Check if email exists, if yes then verify password
+                if(mysqli_stmt_num_rows($stmt) == 1){                    
+                    mysqli_stmt_bind_result($stmt, $id, $employee_id, $name, $email, $hashed_password, $role);
+                    if(mysqli_stmt_fetch($stmt)){
+                        if(password_verify($password, $hashed_password)){
+                            // Password is correct, start a new session
+                            session_start();
+                            
+                            // Store data in session variables
+                            $_SESSION["loggedin"] = true;
+                            $_SESSION["id"] = $id;
+                            $_SESSION["employee_id"] = $employee_id;
+                            $_SESSION["name"] = $name;
+                            $_SESSION["email"] = $email;   
+                            $_SESSION["role"] = $role;
+                            
+                            // Check if remember me is checked
+                            if(!empty($_POST["remember"])) {
+                                // Set cookie for email and hashed password
+                                setcookie("email", $email, time()+(86400*30), "/");
+                                setcookie("hashed_password", password_hash($password, PASSWORD_DEFAULT), time()+(86400*30), "/");
+                            }
+                            
+                            // Redirect user to dashboard
+                            if($role == 1) {
+                                header("location: employer_dashboard.php");
+                            } else {
+                                header("location: employee_dashboard.php");
+                            }
+                        } else{
+                            // Display an error message if password is not valid
+                            $password_err = "The password you entered was not valid.";
+                        }
+                    }
+                } else{
+                    // Display an error message if email doesn't exist
+                    $email_err = "No account found with that email.";
+                }
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+
+            mysqli_stmt_close($stmt);
+        }
+    }
+    
+    mysqli_close($conn);
 }
 ?>
 
