facfb3d326d47d5f1d54210140fc0b8549ece5bc
jkdlps <119383896+jkdlps@users.noreply.github.com>
2023-03-03 02:10:10 +0000

    modified:   employer_add_employee.php 	modified:   pages.txt 	modified:   signup-form.php

--- employer_add_employee.php
+++ employer_add_employee.php
@@ -3,13 +3,74 @@ session_start();
 include "conn.php";
 include "header.php";
 ?>
-
+<!-- 
 <div>
     <h2>Add Employee</h2>
+    <form method="post" action="employer_add_employee.php">
+        <label>Name:</label>
+        <input type="text" name="name" required>
+        <br>
+        <label>Email:</label>
+        <input type="email" name="email" required>
+        <br>
+        <label>Password:</label>
+        <input type="password" name="password" required>
+        <br>
+        <input type="submit" value="Add Employee">
+</form>
 </div>
 
 <div>
     <form action='employer_dashboard.php' method='post'>
         <button type='submit'>Back to Dashboard</button>
     </form>
-</div>
+</div> -->
+
+<?php
+// Initialize variables
+$name = "";
+$email = "";
+$password = "";
+
+// Check if form is submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  // Get form data
+  $name = sanitize($_POST["name"]);
+  $email = sanitize($_POST["email"]);
+  
+  // Generate random password
+  $password = bin2hex(random_bytes(5)); // Generates 10-character random password
+  
+  // Hash the password
+  $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+  
+  // Prepare and bind the statement
+  $stmt = $conn->prepare("INSERT INTO employees (name, email, password) VALUES (?, ?, ?)");
+  $stmt->bind_param("sss", $name, $email, $hashed_password);
+  
+  // Execute the statement
+  if ($stmt->execute()) {
+    echo "Employee added successfully! <br>";
+    echo "Name: " . $name . "<br>";
+    echo "Email: " . $email . "<br>";
+    echo "Password: " . $password . "<br>";
+  } else {
+    echo "Error: " . $stmt->error;
+  }
+  
+  // Close the statement and connection
+  $stmt->close();
+  $conn->close();
+}
+?>
+
+	<h1>Add Employee</h1>
+	<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+		<label>Name:</label>
+		<input type="text" name="name" required>
+		<br>
+		<label>Email:</label>
+		<input type="email" name="email" required>
+		<br>
+		<input type="submit" value="Add Employee">
+	</form>
--- signup-form.php
+++ signup-form.php
@@ -4,19 +4,17 @@ include "header.php";
 ?>
 <h2>Signup</h2>
     <form method="post" action="signup.php">
+        <label>Name:</label>
+        <input type="text" name="name" required>
+        <br>
         <label>Email:</label>
         <input type="email" name="email" required>
         <br>
         <label>Password:</label>
         <input type="password" name="password" required>
         <br>
-        <label>Name:</label>
-        <input type="text" name="name" required>
-        <br>
         <input type="submit" value="Signup">
-    </form>
-</body>
-</html>
+</form>
 
 <div>
     <form action="index.php" method="get">
