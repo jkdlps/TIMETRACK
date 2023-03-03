c21ec0617816b9902581c767b11d27c8e6accb00
jkdlps <119383896+jkdlps@users.noreply.github.com>
2023-03-03 01:39:26 +0000

    modified:   signup.php

--- signup.php
+++ signup.php
@@ -15,38 +15,19 @@ if(isset($_SESSION['user_id'])) {
 // $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
 // $name = $_POST['name'];
 
-// Prepare the SQL statement
-$stmt = $conn->prepare("INSERT INTO users (email, password, name) VALUES (?, ?, ?)");
-
-// Bind parameters to the placeholders
-$stmt->bind_param("sss", $email, $password, $name);
-
 // Get the form data
 $email = sanitize($_POST['email']);
 $password = password_hash(sanitize($_POST['password']), PASSWORD_DEFAULT);
 $name = sanitize($_POST['name']);
-$stmt->execute();
 
-if ($conn->query($sql)) {
-  echo "<script>alert('User created successfully');</script>";
-header('Location: login.php');
+// Insert the user into the database
+$sql = "INSERT INTO users (email, password, name) VALUES ('$email', '$password', '$name')";
+
+if (mysqli_query($conn, $sql)) {
+  echo "User created successfully";
+  header('Location: login.php');
 } else {
-  echo "<script>alert('Error: " . mysqli_error($conn) . " ');</script>";
+  echo "Error: " . mysqli_error($conn);
 }
-
-
-// Close statement and connection
-$stmt->close();
-$conn->close();
-
-// // Insert the user into the database
-// $sql = "INSERT INTO users (email, password, name) VALUES ('$email', '$password', '$name')";
-
-// if (mysqli_query($conn, $sql)) {
-//   echo "User created successfully";
-//   header('Location: login.php');
-// } else {
-//   echo "Error: " . mysqli_error($conn);
-// }
-// mysqli_close($conn);
-// session_unset();
+mysqli_close($conn);
+session_unset();
