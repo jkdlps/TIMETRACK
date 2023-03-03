b95527d59704f47e47f0c1845e637956b226ab2f
jkdlps <119383896+jkdlps@users.noreply.github.com>
2023-03-03 01:27:19 +0000

    modified:   signup.php

--- signup.php
+++ signup.php
@@ -10,13 +10,22 @@ if(isset($_SESSION['user_id'])) {
   }
 }
 
-// Get the form data
-$email = $_POST['email'];
-$password = password_hash($_POST['password'], PASSWORD_DEFAULT);
-$name = $_POST['name'];
+// // Get the form data
+// $email = $_POST['email'];
+// $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+// $name = $_POST['name'];
 
-// Insert the user into the database
-$sql = "INSERT INTO users (email, password, name) VALUES ('$email', '$password', '$name')";
+// Prepare the SQL statement
+$stmt = $conn->prepare("INSERT INTO users (email, password, name) VALUES (?, ?, ?)");
+
+// Bind parameters to the placeholders
+$stmt->bind_param("sss", $email, $password, $name);
+
+// Get the form data
+$email = sanitize($_POST['email']);
+$password = password_hash(sanitize($_POST['password']), PASSWORD_DEFAULT);
+$name = sanitize($_POST['name']);
+$stmt->execute();
 
 if (mysqli_query($conn, $sql)) {
   echo "User created successfully";
@@ -24,5 +33,19 @@ if (mysqli_query($conn, $sql)) {
 } else {
   echo "Error: " . mysqli_error($conn);
 }
-mysqli_close($conn);
-session_unset();
+
+// Close statement and connection
+$stmt->close();
+$conn->close();
+
+// // Insert the user into the database
+// $sql = "INSERT INTO users (email, password, name) VALUES ('$email', '$password', '$name')";
+
+// if (mysqli_query($conn, $sql)) {
+//   echo "User created successfully";
+//   header('Location: login.php');
+// } else {
+//   echo "Error: " . mysqli_error($conn);
+// }
+// mysqli_close($conn);
+// session_unset();
