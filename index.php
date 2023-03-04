2bb3ef511d459ba99667f6b082ac129e4c4db01f
jkdlps <119383896+jkdlps@users.noreply.github.com>
2023-03-04 05:15:07 +0000

    modified:   index.php

--- index.php
+++ index.php
@@ -176,7 +176,7 @@
 
             <!-- Copyright -->
             <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-            © 2020 Copyright:
+            &copy<php? echo date('Y'); ?>Copyright:
             <a class="text-dark" href="https://mdbootstrap.com/">MDBootstrap.com</a>
             </div>
             <!-- Copyright -->
