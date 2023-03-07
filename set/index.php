26b43602b5e09956e85849dec889ee4bb8f07009
jkdlps <119383896+jkdlps@users.noreply.github.com>
2023-03-07 11:03:12 +0000

    modified:   set/index.php

--- set/index.php
+++ set/index.php
@@ -77,14 +77,21 @@
        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
        <meta http-equiv="x-ua-compatible" content="ie=edge">
        <title>Material Design for Bootstrap</title>
-       <!-- Font Awesome -->
-       <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
-       <!-- Google Fonts Roboto -->
-       <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
-       <!-- Bootstrap core CSS -->
-       <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet">
-       <!-- Material Design Bootstrap -->
-       <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/css/mdb.min.css" rel="stylesheet">
+<!-- Font Awesome -->
+<link
+  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
+  rel="stylesheet"
+/>
+<!-- Google Fonts -->
+<link
+  href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+  rel="stylesheet"
+/>
+<!-- MDB -->
+<link
+  href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.2.0/mdb.min.css"
+  rel="stylesheet"
+/>
    </head>
    <body>
        <!-- Start your project here-->
@@ -127,14 +134,10 @@
     </div>
     <!-- Background image -->
        <!-- End your project here-->
-       <!-- jQuery -->
-       <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-       <!-- Bootstrap tooltips -->
-       <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js"></script>
-       <!-- Bootstrap core JavaScript -->
-       <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.min.js"></script>
-       <!-- MDB core JavaScript -->
-       <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/js/mdb.min.js"></script>
-       </script>
+<!-- MDB -->
+<script
+  type="text/javascript"
+  src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.2.0/mdb.min.js"
+></script>
    </body>
 </html>
