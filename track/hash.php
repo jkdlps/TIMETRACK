250996f4538d1a56d327aa84dda8e676cda46d48
jkdlps <jameskevinrempillodelposo@outlook.com>
2023-03-08 19:02:31 +0800

    Update hash.php

--- track/hash.php
+++ track/hash.php
@@ -14,11 +14,18 @@ echo "The hashed password is: " . var_dump($hashed);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hashing Passwords | Timetrack</title>
+
+    <!-- Latest compiled and minified CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 <body>
     <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
     <label for="password">Password: </label>
     <input type="password" name="password" id="password">
+    <input type="submit" value="Hash">
     </form>
 </body>
 </html>
