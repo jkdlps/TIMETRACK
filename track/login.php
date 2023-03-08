be57877729c9d2a51f34328f2d445f45c7603d81
jkdlps <jameskevinrempillodelposo@outlook.com>
2023-03-08 22:33:04 +0800

    Update login.php

--- track/login.php
+++ track/login.php
@@ -59,7 +59,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
             <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
                 <div class="mt-3">
                     <label for="email" class="form-label">Email: </label>
-                    <input type="password" name="password" id="password" class="form-control" required>
+                    <input type="email" name="email" id="email" class="form-control" required>
                 </div>
                 <div class="mt-3">
                     <label for="password" class="form-label">Password: </label>
