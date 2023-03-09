1bc49550b260ba7c27c07e3630069ca51ba92b17
jkdlps <jameskevinrempillodelposo@outlook.com>
2023-03-09 21:10:24 +0800

    Update login.php

--- control/login.php
+++ control/login.php
@@ -1,7 +1,7 @@
 <?php
-// session_start();
-include "functions.php";
-db();
+session_start();
+// include "functions.php";
+$con = mysqli_connect("localhost", "u947188626_timetrack", "|5FnHl7#", "u947188626_timetrack");
 
 if (isset($_POST['submit'])) {
     $email = $_POST['email'];
