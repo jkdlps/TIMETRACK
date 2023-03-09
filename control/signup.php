f217a3625ab484d4bb5e1d9623a94b7ceff1920c
jkdlps <jameskevinrempillodelposo@outlook.com>
2023-03-09 19:01:01 +0800

    Update signup.php

--- control/signup.php
+++ control/signup.php
@@ -6,7 +6,9 @@ if (isset($_POST['submit'])) {
     $email = $_POST['email'];
     $password = $_POST['password'];    
     db();
-    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+    if($_POST['password'] < 8) {
+        $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+    }
     $query = "INSERT INTO users (email, password) VALUES ('$email', '$password')";
     mysqli_query($con, $query);
     mysqli_close($con);
