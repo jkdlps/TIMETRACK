ca4a457693cc6d0a5e35707c8003e77faff8a55d
jkdlps <jameskevinrempillodelposo@outlook.com>
2023-03-09 19:41:55 +0800

    Update index.php

--- index.php
+++ index.php
@@ -21,6 +21,10 @@ head("Home");
 
     </div>
 
+    <div class="container-fluid text-center">
+        <a href="usersindex.php" class="btn btn-primary btn-block">Take Attendance</a>
+    </div>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
     </script>
