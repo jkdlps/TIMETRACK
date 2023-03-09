36e64ee96efe3aaff22eab9455a52f5fa41c148c
jkdlps <jameskevinrempillodelposo@outlook.com>
2023-03-09 20:17:45 +0800

    changes

--- control/login.php
+++ control/login.php
@@ -4,17 +4,15 @@ include "functions.php";
 db();
 
 if (isset($_POST['submit'])) {
-    alerter("logged in!", "success");
     $email = $_POST['email'];
 
     $password = password_verify($_POST['password'], PASSWORD_DEFAULT);
 
     $query = "SELECT * FROM users WHERE email='$email' AND password='$password'";
     $result = mysqli_query($con, $query);
-    alerter("successful database!", "success");
     if (mysqli_num_rows($result) > 0) {
         $_SESSION['email'] = $email;
-        header('Location: dashboard.php');
+        header('Location: employee_dashboard.php');
         exit();
     } else {
         echo 'Invalid email or password';
