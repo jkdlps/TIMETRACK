e84692399dee50eee56306f028bddf0a361e56b6
jkdlps <jameskevinrempillodelposo@outlook.com>
2023-03-09 23:44:14 +0800

    Update login.php

--- loginpage/login.php
+++ loginpage/login.php
@@ -1,4 +1,28 @@
-<div class="col-lg-5 text-center text-lg-start">
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Timetrack</title>
+    <link rel="stylesheet" href="styles.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+
+
+    <link rel="stylesheet" href="styles.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+</head>
+
+<body>
+
+    <div class="container col-xl-10 col-xxl-8 px-4 py-5">
+        <div class="row align-items-center g-lg-5 py-5">
+
+            <div class="col-lg-6 text-center text-lg-start">
                 <img class="img-fluid" src="./images/login.png" />
             </div>
             <div class="col-md-10 mx-auto col-lg-6">
