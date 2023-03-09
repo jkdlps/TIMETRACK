d41bca9ba0f9334c6b55915901bf852a5efd72f0
jkdlps <jameskevinrempillodelposo@outlook.com>
2023-03-09 18:28:32 +0800

    Update login.php

--- control/login.php
+++ control/login.php
@@ -1,14 +1,15 @@
 <?php
 session_start();
 include "functions.php";
+db();
 
 if (isset($_POST['submit'])) {
     $email = $_POST['email'];
     $password = $_POST['password'];
 
-    db();
+    $decoded = password_verify($password, PASSWORD_DEFAULT);
 
-    $query = "SELECT * FROM users WHERE email='$email' AND password='$password'";
+    $query = "SELECT * FROM users WHERE email='$email' AND password='$decoded'";
     $result = mysqli_query($con, $query);
 
     if (mysqli_num_rows($result) > 0) {
