8553febe19328c03d89da466d47965d646659fae
jkdlps <jameskevinrempillodelposo@outlook.com>
2023-03-09 21:50:29 +0800

    a

--- backend/connection.php
+++ backend/connection.php
@@ -1,4 +1,7 @@
 <?php
-include "../control/functions.php";
-db();
+// include "../control/functions.php";
+// db();
+
+$con = mysqli_connect("localhost", "u947188626_timetrack", "|5FnHl7#", "u947188626_timetrack");
+$conn = new mysqli("localhost", "u947188626_timetrack", "|5FnHl7#", "u947188626_timetrack");
 ?>
---
+++ backend/loginBackend.php
@@ -0,0 +1,40 @@
+<?php
+
+include("connection.php");
+
+// Check if form is submitted
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+$email = $_POST['email'];
+$password = $_POST['password'];
+$errors = array();
+
+    // Escape input to prevent SQL injection
+    $email = mysqli_real_escape_string($conn, $email);
+    $password = mysqli_real_escape_string($conn, $password);
+
+    // Query database for user
+    $sql = "SELECT * FROM users WHERE email = '$email' AND password = '$password'";
+    $result = mysqli_query($conn, $sql);
+
+    // Check if user exists
+    if ($result->num_rows > 0) {
+      // output data of each row
+      while ($row = $result->fetch_assoc()) {
+          $_SESSION["login"] = true;
+          $_SESSION["id"] = $row["id"];
+          if($_SESSION['role'] == 0) {
+            header("location: employee_dashboard.php");
+          } else {
+            header("location: admin_dashboard.php");
+          }
+      }
+  }
+    else {
+      echo '<span style="color:#DC0000;text-align:center;">Invalid email or password.</span>';
+    }
+
+    mysqli_close($conn); // Close database connection
+
+}
+
+?>
