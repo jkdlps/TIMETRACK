8df3cf8f5d155a58d57ffe4087732eea8dcc30bf
jkdlps <jameskevinrempillodelposo@outlook.com>
2023-03-09 18:20:04 +0800

    update files

--- loginpage/login.php
+++ loginpage/login.php
@@ -1,14 +1,11 @@
-<!doctype html>
-<html lang="en">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Timetrack</title>
+<?php
+session_start();
+include "../control/functions.php";
+head("Login")
+?>
     <link rel="stylesheet" href="styles.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-
 
     <link rel="stylesheet" href="styles.css">
 
@@ -27,7 +24,7 @@
             </div>
             <div class="col-md-10 mx-auto col-lg-6">
 
-                <form class="p-4 p-md-5  rounded-3">
+                <form class="p-4 p-md-5  rounded-3" action="../control/login.php" >
 
                     <h1 class="my-2">Login</h1>
 
