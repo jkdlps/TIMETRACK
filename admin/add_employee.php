16ab2343d51417724de0a5264e089f0dab173c11
jkdlps <jameskevinrempillodelposo@outlook.com>
2023-03-10 16:41:59 +0800

    admin

--- admin/add_employee.php
+++ admin/add_employee.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
-include "connection.php";
+include "conn.php";
+include "header.php";
 ?>
 <!-- 
 <div>
@@ -27,15 +28,15 @@ include "connection.php";
 
 <div>
     <h1>Add Employee</h1>
-    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
-        <label>Name:</label>
-        <input type="text" name="name" required>
-        <br>
-        <label>Email:</label>
-        <input type="email" name="email" required>
-        <br>
-        <input type="submit" value="Add Employee">
-    </form>
+	<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+		<label>Name:</label>
+		<input type="text" name="name" required>
+		<br>
+		<label>Email:</label>
+		<input type="email" name="email" required>
+		<br>
+		<input type="submit" value="Add Employee">
+	</form>
 </div>
 
 <div>
@@ -52,33 +53,33 @@ $password = "";
 
 // Check if form is submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Get form data
-    $name = sanitize($_POST["name"]);
-    $email = sanitize($_POST["email"]);
-
-    // Generate random password
-    $password = bin2hex(random_bytes(5)); // Generates 10-character random password
-
-    // Hash the password
-    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
-
-    // Prepare and bind the statement
-    $stmt = $conn->prepare("INSERT INTO users (name, email, password) VALUES (?, ?, ?)");
-    $stmt->bind_param("sss", $name, $email, $hashed_password);
-
-    // Execute the statement
-    if ($stmt->execute()) {
-        echo "<script>alert('";
-        echo "Employee added successfully! '<br>";
-        echo "Name: " . $name . '<br>';
-        echo "Email: " . $email . "'<br>";
-        echo "Password: " . $password . "'<br>";
-        echo "');</script>";
-    } else {
-        echo "<script>alert('Error: " . $stmt->error . "');</script>";
-    }
-
-    // Close the statement and connection
-    $stmt->close();
-    $conn->close();
+  // Get form data
+  $name = sanitize($_POST["name"]);
+  $email = sanitize($_POST["email"]);
+  
+  // Generate random password
+  $password = bin2hex(random_bytes(5)); // Generates 10-character random password
+  
+  // Hash the password
+  $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+  
+  // Prepare and bind the statement
+  $stmt = $conn->prepare("INSERT INTO users (name, email, password) VALUES (?, ?, ?)");
+  $stmt->bind_param("sss", $name, $email, $hashed_password);
+  
+  // Execute the statement
+  if ($stmt->execute()) {
+    echo "<script>alert('";
+    echo "Employee added successfully! '<br>";
+    echo "Name: " . $name . '<br>';
+    echo "Email: " . $email . "'<br>";
+    echo "Password: " . $password . "'<br>";
+    echo "');</script>";
+  } else {
+    echo "<script>alert('Error: " . $stmt->error . "');</script>";
+  }
+  
+  // Close the statement and connection
+  $stmt->close();
+  $conn->close();
 }
--- admin/admin_dashboard.php
+++ admin/admin_dashboard.php
@@ -3,13 +3,12 @@ session_start();
 include "../control/connection.php";
 
 if($_SESSION['role'] == 0) {
-  die("
+  exit("
   <script>
     alert('For admins only');
   </script>
   ");
-
-  
 }
 ?>
 
+<a href="add_employee.php" class="btn btn-secondary">Add Employee</a>
