dbd42e2ac589a32de234195c2cee1ec3339b3c49
jkdlps <jameskevinrempillodelposo@outlook.com>
2023-03-10 14:54:11 +0800

    updated signup

--- backend/signupbackend.php
+++ backend/signupbackend.php
@@ -3,9 +3,10 @@ session_start();
 include "../control/connection.php";
 
 if (isset($_POST['submit'])) {
-    $email = $_POST['floatingInput'];
-    $password = $_POST['floatingPassword'];
-    $query = "INSERT INTO users (email, password) VALUES ('$email', '$password')";
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $query = "INSERT INTO users (name, email, password) VALUES ('$name','$email', '$password')";
     mysqli_query($con, $query);
     mysqli_close($con);
     header("location: ../loginpage.php");
--- signuppage/signup.php
+++ signuppage/signup.php
@@ -32,16 +32,16 @@
                     <h1 class="my-3">Signup</h1>
 
                     <div class="form-floating mb-3">
-                        <input type="text" class="form-control" id="floatingInput" placeholder="name@example.com">
-                        <label for="floatingInput">Name</label>
+                        <input type="text" class="form-control" id="name" placeholder="name@example.com">
+                        <label for="name">Name</label>
                     </div>
                     <div class="form-floating mb-3">
-                        <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-                        <label for="floatingInput">Email address</label>
+                        <input type="email" class="form-control" id="email" placeholder="name@example.com">
+                        <label for="email">Email address</label>
                     </div>
                     <div class="form-floating mb-3">
-                        <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-                        <label for="floatingPassword">Password</label>
+                        <input type="password" class="form-control" id="password" placeholder="Password">
+                        <label for="password">Password</label>
                     </div>
                     <div class="checkbox mb-3">
                         <label>
