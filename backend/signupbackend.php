847924d7decb4adaf1914d472a78771169198772
jkdlps <jameskevinrempillodelposo@outlook.com>
2023-03-10 14:48:19 +0800

    Update signupbackend.php

--- backend/signupbackend.php
+++ backend/signupbackend.php
@@ -3,8 +3,8 @@ session_start();
 include "../control/connection.php";
 
 if (isset($_POST['submit'])) {
-    $email = $_POST['email'];
-    $password = $_POST['password'];    
+    $email = $_POST['floatingInput'];
+    $password = $_POST['floatingPassword'];
     $query = "INSERT INTO users (email, password) VALUES ('$email', '$password')";
     mysqli_query($con, $query);
     mysqli_close($con);
