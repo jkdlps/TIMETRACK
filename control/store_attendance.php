484c2c08e79dc2a61bcbc0c5d997c2b5f52a8227
jkdlps <jameskevinrempillodelposo@outlook.com>
2023-03-10 12:45:36 +0800

    Update store_attendance.php

--- control/store_attendance.php
+++ control/store_attendance.php
@@ -67,6 +67,28 @@
             <div class="form-outline mb-4">
                 <label class="form-label" for="time">Time</label>
                 <h3 id="clock"></h3>
+                <script>
+                function updateTime() {
+        var now = new Date();
+        var hours = now.getHours();
+        var minutes = now.getMinutes();
+        var seconds = now.getSeconds();
+
+        // Add leading zeros to the hours, minutes, and seconds
+        hours = hours < 10 ? "0" + hours : hours;
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        // Format the time as "hh:mm:ss"
+        var timeString = hours + ":" + minutes + ":" + seconds;
+
+        // Update the clock element with the new time
+        document.getElementById("clock").innerHTML = timeString;
+    }
+
+    // Call updateTime every second to update the clock
+    setInterval(updateTime, 1000);
+    </script>
             </div>
         </div>
         <div class='col-lg-6'>
