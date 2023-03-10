b6df83d7a832c4427ab899fd901db686d91bc595
jkdlps <jameskevinrempillodelposo@outlook.com>
2023-03-10 15:04:54 +0800

    Update attendancebackend.php

--- backend/attendancebackend.php
+++ backend/attendancebackend.php
@@ -5,14 +5,16 @@ include("connection.php");
 if (isset($_POST['submit'])) {
     $latitude = $_POST['latitude'];
     $longitude = $_POST['longitude'];
-    $time_in = $_POST['time_in'];
-    $time_out = $_POST['time_out'];
+    $time = date('H:i:s');
+    $date = date('Y-m-d');
 
-    $sql = "INSERT INTO location (latitude, longitude, time_in, time_out) VALUES ('$latitude', '$longitude', '$time_in', '$time_out')";
+    $sql = "INSERT INTO attendance (latitude,longitude,time,date )
+    VALUES ('$latitude','$longitude','$time','$date')";
 
     if ($conn->query($sql) === TRUE) {
         $_SESSION['message'] = "Reserved Successfully";
-        header("location: ../control/store_attendance.php");
+        header("location: ../loginpage.php");
+
     } else {
         $_SESSION['message'] = "Wrong Password";
         echo "Error: " . $sql . "<br>" . $conn->error;
