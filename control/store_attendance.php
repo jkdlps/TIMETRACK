fad8d67c8402b5289dfbd8eeffddfedb4e07e5f0
jkdlps <jameskevinrempillodelposo@outlook.com>
2023-03-12 15:03:32 +0800

    added script

--- control/store_attendance.php
+++ control/store_attendance.php
@@ -25,6 +25,11 @@
 </head>
 
 <body onload="getLocation();">
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
+    </script>
+
     <div id="map"></div>
     <?php date_default_timezone_set('Asia/Manila'); ?>
     <script>
