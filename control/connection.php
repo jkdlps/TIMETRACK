55e3074340a637df685b11023094fb4b0f70afa7
jkdlps <jameskevinrempillodelposo@outlook.com>
2023-03-14 19:21:57 +0800

    deploy new database in new domain

--- control/connection.php
+++ control/connection.php
@@ -1,4 +1,4 @@
 <?php
-$con = mysqli_connect("localhost", "u947188626_timetrack", "|5FnHl7#", "u947188626_timetrack");
-$conn = new mysqli("localhost", "u947188626_timetrack", "|5FnHl7#", "u947188626_timetrack");
+$con = mysqli_connect("localhost", "u947188626_0timetrack", "9hpoelF", "u947188626_0timetrack");
+$conn = new mysqli("localhost", "u947188626_0timetrack", "9hpoelF", "u947188626_0timetrack");
 ?>
