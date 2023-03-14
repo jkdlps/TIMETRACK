acad066fbcfeee3ef2200d147f14a52b73b8120e
jkdlps <jameskevinrempillodelposo@outlook.com>
2023-03-14 19:49:11 +0800

    removed danger

--- admin/backend/connection.php
+++ admin/backend/connection.php
@@ -3,13 +3,4 @@
 // "localhost", "u947188626_timetrack", "|5FnHl7#", "u947188626_timetrack"
 $con = mysqli_connect("localhost", "u947188626_0timetrack", "9hpoelF", "u947188626_0timetrack");
 $conn = new mysqli("localhost", "u947188626_0timetrack", "9hpoelF", "u947188626_0timetrack");
-
-function alerter($message) {
-    echo "
-    <script>
-        alert('$message');
-    </script>
-    ";
-}
 ?>
-
