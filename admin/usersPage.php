2eddb4a6365907b785adde1be327de484609f461
jkdlps <jameskevinrempillodelposo@outlook.com>
2023-03-14 19:38:54 +0800

    Update usersPage.php

--- admin/usersPage.php
+++ admin/usersPage.php
@@ -4,7 +4,7 @@ session_start();
 include("includes/header.php");
 include("includes/navbar.php");
 include("includes/sidebar.php");
-include "../backend/message.php";
+// include "../backend/message.php";
 include("components/usersTable.php");
 include("includes/footer.php");
 ?>
