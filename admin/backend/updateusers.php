4086723d23964e55b11578cdf02dd5ad93593281
jkdlps <jameskevinrempillodelposo@outlook.com>
2023-03-14 18:17:43 +0800

    Update updateusers.php

--- admin/backend/updateusers.php
+++ admin/backend/updateusers.php
@@ -1,16 +1,24 @@
 <?php
 session_start();
 include "connection.php";
+include "../TIMETRACK/backend/message.php";
 
 if (isset($_POST['update'])) {
     $id = $_POST['id'];
     $firstname = $_POST['firstname'];
     $lastname = $_POST['lastname'];
     $email = $_POST['email'];
+    $password = $_POST['password'];
     $role = $_POST['role'];
     $designation = $_POST['designation'];
 
-    $sql = "UPDATE users SET firstname='$firstname', lastname='$lastname', email='$email', role='$role', designation='$designation' WHERE id='$id'";
+    if(!($password < 8)) {
+        $sql = "UPDATE users SET firstname='$firstname', lastname='$lastname', email='$email', password='$password', role='$role', designation='$designation' WHERE id='$id'";
+    } else {
+        $_SESSION['message'] = "Password must be at least 8 characters long.";
+        echo "<script>
+        alert('Password must be at least 8 characters long')</script>";
+    }
 
     if ($conn->query($sql) === TRUE) {
         header('Location: ../usersPage.php');
